c8df637a8e57f40540e6876810e53a6d8030fe85
Gaurav Kumar <gauravkr927@gmail.com>
2019-06-02 12:44:38 +0530

    Update Viterbi_problem_2.py

--- Viterbi_problem_2.py
+++ Viterbi_problem_2.py
@@ -1,4 +1,5 @@
-def Solve_problem2(A,B,intial,observation):
+import numpy as np
+def Solve_problem1(A,B,intial,observation):
 	"""
 	A: transition probablity matrix (N X N)
 		aij represent probablity of transition from state i to state j
@@ -6,26 +7,27 @@ def Solve_problem2(A,B,intial,observation):
 		bij represent probablity of emitting jth observation state from ith
 		hidden state
 	intial = probablity of hidden state as being the first 
+
 	Observation : Sequence of obsevation
+
 	"""
 	# forward[t][j] = P(o1,o2. . .ot,qt = j|λ)
 	# forward[t][j] = sum(forward[t-1][i]*aij*bj(ot))
 
-	forward = [[0 for i in range(len(A))] for j in range(len(obsevation))]
+	forward = np.zeros(shape=(len(A),len(observation)))
 	N = len(A)
-	T = len(obsevation)
+	T = len(observation)
 
 	for i in range(N):
-		forward[i][0] = intial[i]*B[i][obsevation[0]]
+		forward[i,0] = intial[i]*B[i][observation[0]]
 
-	
 
 	for t in range(1,T):
 		for j in range(N):
-			sol =0
+			sol =0.0
 			for i in range(N):
-				sol +=forward[t-1][i]*A[i][j]*B[j][obsevation[t]]
-			forward[t][j] =sol
+				sol +=forward[i,t-1]*A[i][j]*B[j][observation[t]]
+			forward[j,t] =sol
 
 	state = []
 
@@ -40,7 +42,3 @@ def Solve_problem2(A,B,intial,observation):
 		state.append(step)
 
 	return state
-
-
-
-	
